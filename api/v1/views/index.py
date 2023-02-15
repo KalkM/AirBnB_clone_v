558f231ccb64d3a1c165d36ac35502972706d888
KalkM <106752167+KalkM@users.noreply.github.com>
2023-02-15 22:12:43 +0300

    index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,25 +1,28 @@
 #!/usr/bin/python3
-"""function to create the route status"""
+"""Creationg route for Blueprint
+"""
 from api.v1.views import app_views
-from flask import jsonify
 from models import storage
-from flasgger import swag_from
+from flask import jsonify
 
 
 @app_views.route('/status')
-@swag_from('./apidocs/status.yml')
-def status():
-    """status render template for json"""
-    return jsonify({"status": "OK"})
+def response():
+    """ get status ok
+    """
+    dic = {"status": "OK"}
+    return jsonify(dic)
 
 
 @app_views.route('/stats')
-@swag_from('./apidocs/stats.yml')
-def stats():
-    """status render template for json"""
-    dict_objs = {'amenities': 'Amenity', 'cities': 'City', 'places': 'Place',
-                 'reviews': 'Review', 'states': 'State', 'users': 'User'}
-    new_dict = {}
-    for k, v in dict_objs.items():
-        new_dict[k] = storage.count(v)
-    return jsonify(new_dict)
+def class_counter():
+    """ get a dictionary from count method
+    """
+    dic = {}
+    dic["amenities"] = storage.count("Amenity")
+    dic["cities"] = storage.count("City")
+    dic["places"] = storage.count("Place")
+    dic["reviews"] = storage.count("Review")
+    dic["states"] = storage.count("State")
+    dic["users"] = storage.count("User")
+    return jsonify(dic)
