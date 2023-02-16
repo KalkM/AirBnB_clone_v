9ed9370120fcd322eb9a392b85766714ae50e312
KalkM <106752167+KalkM@users.noreply.github.com>
2023-02-16 17:09:28 +0300

    app

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,37 +1,43 @@
 #!/usr/bin/python3
 """
-A python script that starts a Flask web application
+This module contains the principal application
 """
-from flask import Flask, Blueprint, abort, jsonify
 from models import storage
 from api.v1.views import app_views
+from flask import Flask, make_response, jsonify
 from os import getenv
 from flask_cors import CORS
-
+from flasgger import Swagger
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-
-
-@app.errorhandler(404)
-def page_not_found(error):
-    """
-    Function that shows a 404 error
-    """
-    return (jsonify(error="Not found"), 404)
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown(exception=None):
-    """
-     Function closes the current session
-    """
+def close_db(obj):
+    """ calls methods close() """
     storage.close()
 
 
-if __name__ == '__main__':
-    h = getenv('HBNB_API_HOST')
-    p = getenv('HBNB_API_PORT')
-    app.run(host=h, port=p, threaded=True)
+@app.errorhandler(404)
+def page_not_foun(error):
+    """ Loads a custom 404 page not found """
+    return make_response(jsonify({"error": "Not found"}), 404)
+
+
+app.config['SWAGGER'] = {
+    'title': 'AirBnB clone - RESTful API',
+    'description': 'This is the api that was created for the hbnb restful api project,\
+    all the documentation will be shown below',
+    'uiversion': 3}
+
+Swagger(app)
+
+if __name__ == "__main__":
+
+    host = getenv('HBNB_API_HOST', default='0.0.0.0')
+    port = getenv('HBNB_API_PORT', default=5000)
+
+    app.run(host, int(port), threaded=True)
